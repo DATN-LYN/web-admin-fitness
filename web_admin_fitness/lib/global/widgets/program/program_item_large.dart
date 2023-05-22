ceb4467fac40f0bf8c598c48e8e8bc4097e9ace9
Nhi Luu <nhi.luu@team.enouvo.com>
2023-05-22 17:58:00 +0700

    feat/home: init home UI

---
+++ web_admin_fitness/lib/global/widgets/program/program_item_large.dart
@@ -0,0 +1,115 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+import 'package:ionicons/ionicons.dart';
+import 'package:web_admin_fitness/global/routers/app_router.dart';
+
+import '../../enums/workout_body_part.dart';
+import '../../enums/workout_level.dart';
+import '../../gen/i18n.dart';
+import '../../graphql/fragment/__generated__/program_fragment.data.gql.dart';
+import '../shadow_wrapper.dart';
+import '../shimmer_image.dart';
+import '../tag.dart';
+
+class ProgramItemLarge extends StatelessWidget {
+  const ProgramItemLarge({
+    super.key,
+    required this.program,
+  });
+
+  final GProgram program;
+
+  @override
+  Widget build(BuildContext context) {
+    final i18n = I18n.of(context)!;
+    final level = WorkoutLevel.getLevel(program.level ?? 0);
+    final bodyPart = WorkoutBodyPart.getBodyPart(program.bodyPart ?? 0);
+
+    return ShadowWrapper(
+      padding: const EdgeInsets.all(16),
+      borderRadius: BorderRadius.circular(20),
+      child: InkWell(
+        onTap: () => context.pushRoute(
+          ProgramUpsertRoute(program: program),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ShimmerImage(
+              imageUrl: program.imgUrl ?? '_',
+              height: 150,
+              width: double.infinity,
+              fit: BoxFit.fill,
+              borderRadius: BorderRadius.circular(12),
+            ),
+            const SizedBox(height: 16),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  program.name ?? '_',
+                  style: const TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                Tag(
+                  text: level.label(i18n),
+                  color: level.color(),
+                )
+              ],
+            ),
+            const SizedBox(height: 6),
+            _infoTile(
+              icon: Ionicons.body,
+              label: '${i18n.programs_BodyPart}: ',
+              value: bodyPart.label(i18n),
+            ),
+            _infoTile(
+              icon: Ionicons.document_text,
+              label: '${i18n.programs_Description}: ',
+              value: program.description ?? '_',
+            ),
+            _infoTile(
+              icon: Ionicons.eye,
+              label: 'View: ',
+              value: '${program.view?.toInt() ?? 0}',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _infoTile({
+    required String label,
+    required String value,
+    required IconData icon,
+  }) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 4),
+      child: Row(
+        children: [
+          Icon(
+            icon,
+            size: 16,
+          ),
+          const SizedBox(width: 8),
+          Text(
+            label,
+            style: const TextStyle(
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          Expanded(
+            child: Text(
+              value,
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ web_admin_fitness/lib/modules/main/modules/home/widgets/ages_chart.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
+
+import '../../../../../global/themes/app_colors.dart';
+
+class AgesChart extends StatefulWidget {
+  const AgesChart({
+    super.key,
+    required this.ages,
+  });
+  final Map<int, int> ages;
+
+  @override
+  State<AgesChart> createState() => _AgesChartState();
+}
+
+class _AgesChartState extends State<AgesChart> {
+  @override
+  void initState() {
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {});
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SfCircularChart(
+      title: ChartTitle(text: 'Age Users'),
+      legend: Legend(isVisible: true),
+      series: [
+        PieSeries<int, String>(
+          dataSource: widget.ages.entries.map((e) => e.value).toList(),
+          xValueMapper: (data, index) =>
+              widget.ages.keys.elementAt(index).toString(),
+          yValueMapper: (data, _) => data,
+          dataLabelMapper: (data, _) =>
+              '${(data * 100 / widget.ages.length).round()} %',
+          startAngle: 90,
+          endAngle: 90,
+          strokeColor: AppColors.white,
+          strokeWidth: 0.2,
+          dataLabelSettings: const DataLabelSettings(
+            isVisible: true,
+            textStyle: TextStyle(
+              fontWeight: FontWeight.w600,
+              fontSize: 16,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
